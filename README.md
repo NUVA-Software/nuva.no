c2c3da87b90410dc317f3503425c26e162696458
Bjørnar Hagen <bjornar@datahjelpen.no>
2024-02-12 17:19:39 +0100

    feat: update styling

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# nuva.no
+# NUVA Homepage
 
-The NUVA Software homepage.
+[https://nuva.no](https://nuva.no)
+
+## Develop
+
+1. Run `npx -y browser-sync --files *.html`
+2. Open `http://localhost:3000`
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Butler", "serif"],
+      },
+    },
+  },
+};
